f0dbcf361ddfb9926628fd80bae9e60cca34cdab
Jim-Lii <343347795@tdsb.ca>
2026-03-10 14:59:31 +0000

    ghcfcgjcyjgyukvhjk

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -5,5 +5,23 @@ public class PracticeProblem {
 	}
 
 	//Create your functions here
+	public static int sum(int num1, int num2){
+		int sum = num1 + num2;
+		return sum;
+	}
 
+	public static int difference(int num1, int num2){
+		int difference = num1 - num2;
+		return difference;
+	}
+
+	public static double product(double fracNum1, double fracNum2){
+		double product = fracNum1 * fracNum2;
+		return product;
+	}
+
+	public static String removeFirst(String str){
+		String strNoFirst = str.substring(1);
+		return strNoFirst;
+	}
 }
